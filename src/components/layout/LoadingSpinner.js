12383879280ab322feb4dfd6d1762ce9ce5db8ed
OscarBackman92 <jan.oscar.backman@gmail.com>
2024-11-01 13:53:43 +0100

    feat: Add loading spinner component

--- src/components/layout/LoadingSpinner.js
+++ src/components/layout/LoadingSpinner.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { Spinner } from 'react-bootstrap';
+import { Spinner, Container } from 'react-bootstrap';
 
-const LoadingSpinner = () => {
-  return (
-    <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
-      <Spinner animation="border" role="status">
+const LoadingSpinner = ({ fullPage = true }) => {
+  const spinner = (
+    <div className="d-flex justify-content-center align-items-center">
+      <Spinner animation="border" role="status" variant="primary">
         <span className="visually-hidden">Loading...</span>
       </Spinner>
     </div>
+  );
+
+  if (!fullPage) {
+    return spinner;
+  }
+
+  return (
+    <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: '60vh' }}>
+      {spinner}
+    </Container>
   );
 };
 
